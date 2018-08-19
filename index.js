107b62f8e38d0fa7f2dcbc7c3b53a6fff2a10b61
Jared Allard <jaredallard@outlook.com>
2018-08-18 22:15:05 -0700

    chore: eslint stuff

--- index.js
+++ index.js
@@ -6,21 +6,20 @@
  * @version 1
  */
 
-const _      = require('lodash')
-const debug  = require('debug')('media:converter')
-const dyn    = require('triton-core/dynamics')
+const debug = require('debug')('media:converter')
+const dyn = require('triton-core/dynamics')
 const Config = require('triton-core/config')
-const kue    = require('kue')
-const queue  = kue.createQueue({
+const kue = require('kue')
+const queue = kue.createQueue({
   redis: dyn('redis')
 })
 
-const Event  = require('events').EventEmitter
-const event  = new Event()
+const Event = require('events').EventEmitter
+const event = new Event()
 
 debug('init', Date.now())
 
-const init   = async () => {
+const init = async () => {
   const config = await Config('media')
 
   await require('./lib/reciever')(config, queue, event)
