58a809fe4df01c7ab4e1dae02da0d294e9115804
Jared Allard <jaredallard@outlook.com>
2018-08-21 11:50:12 -0700

    feat: quick season support

--- index.js
+++ index.js
@@ -7,22 +7,14 @@
  */
 
 const debug = require('debug')('media:converter')
-const dyn = require('triton-core/dynamics')
 const Config = require('triton-core/config')
-const kue = require('kue')
-const queue = kue.createQueue({
-  redis: dyn('redis')
-})
-
-const Event = require('events').EventEmitter
-const event = new Event()
 
 debug('init', Date.now())
 
 const init = async () => {
   const config = await Config('media')
 
-  await require('./lib/reciever')(config, queue, event)
+  await require('./lib/reciever')(config)
 }
 
 init()
---
+++ tests/helpers/upload.js
@@ -0,0 +1,32 @@
+const request = require('request-promise-native')
+const fs = require('fs')
+const path = require('path')
+
+const file = process.argv[2]
+
+console.log('uploading', file)
+
+const init = async function() {
+  try {
+    const response = await request({
+      url: "http://127.0.0.1:8001/v1/media/1",
+      method: 'PUT',
+      formData: {
+        file: {
+          value: fs.createReadStream(file),
+          options: {
+            filename: path.basename(file),
+            contentType: 'video/x-matroska'
+          }
+        }
+      }
+    })
+
+    console.log('got', response.body)
+  } catch(err) {
+    console.log('err', err.message)
+    process.exit(1)
+  }
+}
+
+init()
